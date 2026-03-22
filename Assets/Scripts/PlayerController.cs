9e147c1f1155e85ad226c991b54bd7f84334ad7e
zira <timoshchavlada@gmail.com>
2026-03-23 00:58:19 +0200

    Movement update + jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,49 @@
+using System;
+using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction MoveAction;
-    public float speed = 0.01f;
+    private Rigidbody2D rb2d;
     private SpriteRenderer sprite;
+
+    public InputAction MoveAction;
+    public InputAction JumpAction;
+
+    public float speed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
+        JumpAction.Enable();
         sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isGrounded = Math.Abs(rb2d.linearVelocityY) < 0.1f;
+        bool jump = JumpAction.WasPressedThisFrame();
+
+        if (jump && isGrounded)
+        {
+            rb2d.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
         Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * speed;
-        transform.position = position;
+
+        Vector2 velocity = rb2d.linearVelocity;
+        velocity.x = move.x * speed;
+        rb2d.linearVelocity = velocity;
 
         if (move.x > 0)
         {
