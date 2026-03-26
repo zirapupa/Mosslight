533e7161929c6f53484f9f48c54541c3fd05c31b
zira <timoshchavlada@gmail.com>
2026-03-27 00:41:52 +0200

    Movement logic improvement and camera fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,49 +1,88 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
-    private SpriteRenderer sprite;
+    [Header("Input References")]
+    [SerializeField]
+    private InputActionReference moveAction;
+    [SerializeField]
+    private InputActionReference jumpAction;
 
-    public InputAction MoveAction;
-    public InputAction JumpAction;
+    [Header("Walk Settings")]
+    [SerializeField]
+    private float walkSpeed = 7f;
+    [SerializeField]
+    private float acceleration = 5f;
+    [SerializeField]
+    private float deceleration = 15f;
 
-    public float MoveSpeed = 5f;
-    public float JumpForce = 5f;
+    [Header("Jump Settings")]
+    [SerializeField]
+    private float jumpHeight = 2.5f;
+    [SerializeField]
+    private LayerMask groundLayer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D _rb;
+    private Collider2D _collider;
+    private Vector2 _moveInput;
+    private bool _isGrounded;
+
+    private void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
-
-        MoveAction.Enable();
-        JumpAction.Enable();
+        _rb = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        bool isGrounded = Math.Abs(rb2d.linearVelocityY) < 0.1f;
-        bool jump = JumpAction.WasPressedThisFrame();
+        moveAction.action.performed += HandleWalkInput;
+        moveAction.action.canceled += HandleWalkInput;
+        jumpAction.action.performed += HandleJumpInput;
+    }
 
-        if (jump && isGrounded)
+    private void OnDisable()
+    {
+        moveAction.action.performed -= HandleWalkInput;
+        moveAction.action.canceled -= HandleWalkInput;
+        jumpAction.action.performed -= HandleJumpInput;
+    }
+
+    private void HandleJumpInput(InputAction.CallbackContext ctx)
+    {
+        if (_isGrounded)
         {
-            rb2d.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            var actualGravity = Mathf.Abs(Physics2D.gravity.y) * _rb.gravityScale;
+            _rb.linearVelocityY = Mathf.Sqrt(2 * jumpHeight * actualGravity);
         }
     }
 
-    private void FixedUpdate()
+    private void HandleWalkInput(InputAction.CallbackContext ctx)
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        _moveInput = ctx.ReadValue<Vector2>();
+    }
 
-        Vector2 velocity = rb2d.linearVelocity;
-        rb2d.linearVelocity = new Vector2(move.x * MoveSpeed, rb2d.linearVelocityY);
-        if (move.x > 0)
-            sprite.flipX = true;
-        else if (move.x < 0)
-            sprite.flipX = false;
+    private void Update()
+    {
+        _isGrounded = GroundCheck();
+        HandleWalking();
+    }
+
+    private void HandleWalking()
+    {
+        var change = _moveInput.x != 0 ? acceleration : deceleration;
+        _rb.linearVelocityX = Mathf.Lerp(_rb.linearVelocityX, _moveInput.x * walkSpeed, Time.deltaTime * change);
+    }
+
+    private bool GroundCheck()
+    {
+        var colliderBottomCenter = new Vector2(
+            _collider.bounds.center.x,
+            _collider.bounds.min.y);
+
+        return Physics2D.OverlapBox(
+            colliderBottomCenter,
+            new Vector2(_collider.bounds.size.x - 0.1f, 0.1f), 0, groundLayer);
     }
 }
